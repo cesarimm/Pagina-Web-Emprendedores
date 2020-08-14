dcd2d8c90355d416a2cf733441e87530bb94d2a8
Cesar Ivan Mtz <bmwcesarim@gmail.com>
2020-08-14 17:24:40 -0500

    Curso Final JAVASCRIPT

--- cotizacion.js
+++ cotizacion.js
@@ -1,10 +1,17 @@
 function guardar(){
 
+
 	//Declaración de las variables
 	 var varNombre = $('#nombre').val();
      var varEmail = $('#email').val();
      var varEstado = $('#estado').val();
      var varTelefono = $('#telefono').val();
 
-	alert("HOLA: "+varNombre);
+     if(varNombre.length == 0){
+     	console.log("Vacia");
+     }else{
+     	alert("HOLA: "+varNombre);
+     }
+
+	
 }
